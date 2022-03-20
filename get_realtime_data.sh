af27a936f51895461c5af697636034e53d4fbf0e
Erik Kurcz <ejkucz@gmail.com>
2022-03-19 20:44:50 -0400

    add scripts for handling and getting data

---
+++ get_realtime_data.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# iter the line extensions and get files
+
+for ext in ace bdfm g jz nqrw l si "" 
+do
+    NOW=$(date +%H%M)
+    filename_ext=$ext
+    if [[ $ext == "" ]]
+    then
+        ext="$ext"
+        filename_ext="1234567"
+    else
+        ext="-$ext"
+    fi
+
+    curl -H @headers --output data/gtfs_data_${filename_ext}_${NOW} https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs${ext}
+    sleep 1
+    echo "Got data for ${filename_ext}"
+done
+
