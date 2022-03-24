a52ff081bf89f4afecd804f94e3aba3b9997ac78
Erik Kurcz <ejkucz@gmail.com>
2022-03-24 13:50:03 -0400

    update script to run on files with stops datags

--- parse_files_in_data_dir.bash
+++ parse_files_in_data_dir.bash
@@ -4,6 +4,6 @@ rm -f current_status_all_lines
 
 for file in $(ls data)
 do 
-    ./mta -f data/$file >> current_status_all_lines
+    ./mta -f data/$file -s mta_static_data/stops.txt >> current_status_all_lines
 done
 
