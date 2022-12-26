cac4fe4855d9a1396c44323ea00e6a21cfc0a42b
Erik Kurcz <ejkucz@gmail.com>
2022-12-26 14:19:41 -0500

    misc for bash script

--- parse_files_in_data_dir.bash
+++ parse_files_in_data_dir.bash
@@ -3,7 +3,6 @@
 rm -f current_status_all_lines
 
 ALL_FILES=""
-
 for file in $(ls data)
 do 
     ALL_FILES="$ALL_FILES -f data/$file" 
@@ -11,3 +10,4 @@ done
 
 bash -c "./mta $ALL_FILES -s mta_static_data/stops.txt >> current_status_all_lines"
 
+echo "done"
