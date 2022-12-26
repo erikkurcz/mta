d1aec0d134eea1425c4c573a158c29e459d98b45
Erik Kurcz <ejkucz@gmail.com>
2022-12-26 14:11:57 -0500

    use carriage returns when getting realtime data

--- get_realtime_data.sh
+++ get_realtime_data.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # iter the line extensions and get files
+STARTSTRING="Got data for:"
+OUTLINE=$STARTSTRING
 
 for ext in ace bdfm g jz nqrw l si "" 
 do
@@ -16,6 +18,14 @@ do
 
     curl -s -H @headers --output data/gtfs_data_${filename_ext}_${NOW} https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs${ext}
     sleep 1
-    echo "Got data for ${filename_ext}"
-done
+    if [[ $OUTLINE == $STARTSTRING ]]
+    then
+        OUTLINE="$OUTLINE ${filename_ext}"
+    else
+        OUTLINE="$OUTLINE, ${filename_ext}"
+    fi
 
+    printf "\r$OUTLINE"
+    
+done
+printf "\n"
