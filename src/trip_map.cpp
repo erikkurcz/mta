18e165b874355d6461fafdb65013889a0d69085c
Erik Kurcz <ejkucz@gmail.com>
2022-12-26 15:31:39 -0500

    clean up logic for adding trips to tiv, now is de-duped and order is right

--- src/trip_map.cpp
+++ src/trip_map.cpp
@@ -1,7 +1,6 @@
 // Structure used to hold all current trip info 
 
-#include <iostream>
-
+#include <iostream> 
 #include "trip_map.hh"
 
 
@@ -10,7 +9,7 @@ bool TripMap::add_trip(TripInfo new_ti)
     if (k_trip_map.count(new_ti.trip_id) == 0)
     {
         // Doesn't exist, so just add it
-        std::cout << "Adding trip info for new trip_id: " << new_ti.trip_id << std::endl;
+        std::cout << "add_trip(): Adding trip info for new trip_id: " << new_ti.trip_id << std::endl;
         TripInfoVec tiv;
         tiv.push_back(new_ti);
         k_trip_map[new_ti.trip_id] = tiv;
@@ -19,11 +18,12 @@ bool TripMap::add_trip(TripInfo new_ti)
     else 
     {
         // Does exist - that is, a TripInfoVec for this TripId exists
-        // We are therefore receiving a second update for an existing trip,
+        // We are therefore receiving a second or subsequent update for an existing trip,
         // and will store it in chronological order relative to others
         // This will be rare when parsing a single file
-        // But if using multiple files, will see many subsequent updates
-        // ie daemon usage
+        // But if using multiple files, will see many subsequent updates, and many duplicates too!
+        // This is because trains only provide updates when in motion, which is unpredictable
+        // We have to dedupe them
 
         // Let's insert the record into the correct spot in the vector
         std::cout << "Adding additional trip info for existing trip_id: " << new_ti.trip_id << std::endl;
@@ -32,35 +32,46 @@ bool TripMap::add_trip(TripInfo new_ti)
         // Find correct spot
         // Guaranteed at least 1 item here
         TripInfoVec::iterator itr = tiv_existing.begin();
+        TripInfo tmp;
         while (itr != tiv_existing.end())
         {
-            TripInfo tmp = *itr;
-            if (tmp.pi.timestamp > new_ti.pi.timestamp)
-            { 
-                // Iter to next location, this isn't where we should insert
-                std::cout << "Additional TripInfo: itering again on timestamp:\n" 
-                          << "tmp:\t" << tmp << "\nnew:\t" << new_ti << std::endl;
-                itr++;
-            }
-            else
+            tmp = *itr;
+            // Check if proposed record has newer timestamp than first record in the TIV
+            if (tmp.pi.timestamp < new_ti.pi.timestamp)
             {
-                // Check if duplicate
+                std::cout << "add_trip(): Inserting at front: new_ti is more recent than most recent existing ti: \n\t" 
+                          << tmp << "\n\tvs new_ti:\n\t" << new_ti << std::endl;
+                tiv_existing.insert(itr, new_ti);
+                return true;
+            } 
+            else if (tmp.pi.timestamp == new_ti.pi.timestamp)
+            { 
+                // Records have the same exact timestamp, likely duplicate record, so check it
                 if (new_ti == tmp){
-                    std::cout << "Duplicate trip, discarding\n" 
+                    std::cout << "add_trip(): Duplicate trip, discarding\n" 
+                              << "tmp:\t" << tmp << "\nnew:\t" << new_ti << std::endl;
+                    return false;
+                } else {
+                    // Should never happen, but just in case...going to log it
+                    std::cerr << "add_trip(): Timestamps match, but TripInfo structs are different!\n\t"
                               << "tmp:\t" << tmp << "\nnew:\t" << new_ti << std::endl;
                     itr++;
                 }
-                else
-                {
-                    std::cout << "Found insert point:\n\t" << tmp << "\n\tfor new_ti:\n\t" << new_ti << std::endl;
-                    // Will have not itered if it is newest update
-                    tiv_existing.insert(itr, new_ti);
-                    break;
-                }
-
+            } 
+            else 
+            {
+                // new_ti is just at an older timestamp than this record, so progress onward
+                // Iter to next location, this isn't where we should insert
+                std::cout << "add_trip(): Additional TripInfo: itering again on timestamp:\n" 
+                          << "tmp:\t" << tmp << "\nnew:\t" << new_ti << std::endl;
+                itr++;
             }
-        }
 
+        }
+        
+        std::cout << "add_trip(): Inserting at end, iteration complete: \n\t" << tmp << "\n\tfor new_ti:\n\t" << new_ti << std::endl;
+        // Will have not itered if it is newest update
+        tiv_existing.insert(itr, new_ti);
 
         return true;
     }
